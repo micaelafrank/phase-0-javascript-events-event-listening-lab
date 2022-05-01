1e30199b1ed3c38a8fa4706caf6531731d1faca8
Micaela Frank <micaelalfrank@gmail.com>
2022-04-30 21:41:54 -0400

    commit tree 3b9cc0314e03921ff0d8b3c6ee068016050f491e

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert);
+}
+
+// addingEventListener(); 
+
+function clickAlert() {
+    alert('I was clicked!');
 }
